871e1369b377acb623714d588aa7a2f1f6286c02
Jamie Sullivan <jkcrawshaw@gmail.com>
2021-01-02 12:01:34 -0500

    adds Ticket and Beverage class, greeting object

---
+++ Beverage.rb
@@ -0,0 +1,11 @@
+class Beverage
+    attr_accessor :name, :price, :calories
+    def initialize(name, price, calories)
+      @name = name
+      @price = price
+      @calories = calories
+    end
+end
+
+
+
---
+++ Ticket.rb
@@ -0,0 +1,8 @@
+class Ticket
+    def event
+        "behavior will be added later ... "
+    end
+end
+
+ticket = Ticket.new
+puts ticket.event
